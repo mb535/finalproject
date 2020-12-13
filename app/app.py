1b5daf865a247be8027f95aa0fd2ab812b39e93d
meahesachin <meahesachin@gmail.com>
2020-12-12 22:08:34 -0500

    Feature: Updated code to handle successful login
    Successful login now takes a user to the index page.

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+import sys
 from utilities import sendemail
 
 
@@ -44,13 +45,33 @@ def form_edit_get(player_id):
     return render_template('edit.html', title='Edit Form', player=result[0])
 
 
-@app.route('/checklogin/<string:email>', methods=['POST'])
-def form_check_login(email):
+@app.route('/checklogin', methods=['POST'])
+def form_check_login():
+
+    strEmail = str(request.form.get('email'))
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblUsers WHERE userEmail=%s', email)
-    request.form.get('pwd')
-    result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', player=result[0])
+    cursor.execute('SELECT * FROM tblUsers WHERE userEmail=%s', strEmail)
+
+    row_count = cursor.rowcount
+    if row_count == 0:
+        print('No rows returned', file=sys.stderr)
+        return render_template('signup.html', title='Signup')
+    else:
+        result = cursor.fetchall()
+
+        if str(result[0]['userPassword']) == str(request.form.get('pswd')):
+
+            user = {'username': str(result[0]['userName'])}
+            cursor = mysql.get_db().cursor()
+            cursor.execute('SELECT * FROM tblPlayersImport')
+            result = cursor.fetchall()
+            return render_template('index.html', title='Home', user=user, players=result)
+
+        else:
+            print('In Else', file=sys.stderr)
+            return render_template('login.html', title='Login Page')
+
+    # return render_template('edit.html', title='Edit Form', player=result[0])
 
 
 @app.route('/edit/<int:player_id>', methods=['POST'])
@@ -73,8 +94,9 @@ def form_insert_get():
 
 @app.route('/index', methods=['GET'])
 def showindex():
-    user = {'username': 'players Project'}
-    sendemail.sendemail('sa247@njit.edu')
+
+    user = {'username': 'Players Project'}
+    # sendemail.sendemail('sa247@njit.edu')
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM tblPlayersImport')
     result = cursor.fetchall()
